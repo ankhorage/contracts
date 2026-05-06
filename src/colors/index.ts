8a425ea7b7da99c4c56d29444308b1a0cf288e93
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-06 18:18:24 +0200

    refactor(theme): keep color engine out of contracts

--- src/colors/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './contrast';
-export * from './harmony';
-export * from './hex';
-export * from './neutral';
-export * from './semantics';
-export * from './swatches';
-export * from './theme-config';
