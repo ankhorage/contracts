3b9f79fe074b3dd708c228748ef0547509f8c3b1
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-19 19:48:43 +0200

    feat(data): add data-source id contracts

---
+++ src/data/ids.ts
@@ -0,0 +1,6 @@
+export type DataSourceId = string;
+export type EndpointId = string;
+export type OperationId = string;
+export type SchemaId = string;
+export type CredentialId = string;
+export type AdapterId = string;
