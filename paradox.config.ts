8deb46ddbd2a00f1d49871b2d1649b28c6085947
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-12 19:12:27 +0200

    chore: use package metadata for Paradox docs

--- paradox.config.ts
+++ paradox.config.ts
@@ -3,10 +3,6 @@ import { defineParadoxConfig } from '@ankhorage/paradox';
 export default defineParadoxConfig({
   mode: 'write',
 
-  docs: {
-    title: 'CONTRACTS',
-  },
-
   package: {
     root: '.',
     entrypoints: ['src/index.ts'],
