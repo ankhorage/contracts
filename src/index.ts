69aeac86e1e76463f0e29af2fa756eec1af40b45
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 05:29:07 +0200

    refactor(contracts): export collection semantics

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './appManifest';
 export * from './auth';
 export * from './bindings';
 export * from './cli';
+export * from './collections';
 export * from './data';
 export * from './db';
 export * from './deploy';
